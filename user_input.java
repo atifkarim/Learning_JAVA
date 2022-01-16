ae588d4e2480636233e963812670eb62ad6ae3ea
atifkarim <atif0902082@gmail.com>
2022-01-16 19:52:14 +0100

    take user input

---
+++ user_input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class user_input {
+    public static void main(String[] args) {
+        System.out.println("HI !! Who are you???");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, I am " + name);
+    }
+
+}
