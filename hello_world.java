f08fd3d7dc121ecfcc3ff7abb83fee6de5a6d9d0
atifkarim <atif0902082@gmail.com>
2022-01-16 18:49:45 +0100

    use case of string args[]

---
+++ hello_world.java
@@ -0,0 +1,11 @@
+// In this code we have learned what is the use case
+// of String args [] and System.out.println
+// To compile it use : javac hello_world.java
+// To run it use : java hello_world
+
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello," + args[0]);
+    }
+}
