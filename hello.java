e4202efde8ba822c028ff8de7c2aecabd71967f6
atifkarim <atif0902082@gmail.com>
2022-01-16 16:22:41 +0100

    first java program
    - file name and public class name will be same

---
+++ hello.java
@@ -0,0 +1,10 @@
+/// file name should be as like as the public class name
+/// here now file name is hello.java and public class name is hello
+/// It is case sensitive
+
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
