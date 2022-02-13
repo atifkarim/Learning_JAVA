633fb3bb92d88c2377cfdada316779b634465970
atifkarim <atif0902082@gmail.com>
2022-02-13 11:58:30 +0100

    parsing and value of int

--- j_4_int_Integer.java
+++ j_4_int_Integer.java
@@ -9,6 +9,15 @@ public class j_4_int_Integer {
         Integer y = new Integer(45); // wrapper class
         System.out.println("Value of x: " + x);
         System.out.println("Value of y: " + y);
+
+
+        String age = "27";
+         // convert string to int. if string is not a number, it will throw an exception. primitive type
+        int age_int = Integer.parseInt(age);
+        System.out.println("age_int: " + age_int);
+        // convert string to Integer, non-primitive
+        Integer age_Integer = Integer.valueOf(age);
+        System.out.println("age_Integer: " + age_Integer);
     }
 
 }
