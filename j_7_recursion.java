cc130e7feb16b04c5090e86431678f3da91ca5ca
atifkarim <atif0902082@gmail.com>
2022-03-13 16:53:00 +0100

    used biginteger to get factorial

--- j_7_recursion.java
+++ j_7_recursion.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class j_7_recursion {
 
-    static int do_recursion(int value) {
+    public static BigInteger do_recursion(int value) {
+        BigInteger factorial_1 = BigInteger.ONE;
+        BigInteger factorial_2 = BigInteger.valueOf(value);
+
         if (value == 0 || value == 1) {
-            return 1;
+            return factorial_1;
         }
         else {
-            return value * do_recursion(value - 1);
+            return factorial_2.multiply(do_recursion(value - 1));
         }
       }
 
@@ -17,14 +21,15 @@ public class j_7_recursion {
         System.out.print("Enter a positive integer: ");
         Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
         int a = sc.nextInt();
+        BigInteger b = BigInteger.valueOf(a);
+        System.out.println("BigInteger Value: " + b);;
 
         if(a < 0) {
             System.out.println("The number is negative");
             System.exit(0);
         }
         else {
-            int result = do_recursion(a);
-            System.out.println("Result is: "+ result);
+            System.out.println("Result is: "+ do_recursion(b.intValue()));
         }
     }
 }
