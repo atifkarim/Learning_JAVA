b758d59d70d986a4da817f7bbc2539a7e00b7e18
atifkarim <atif0902082@gmail.com>
2023-04-16 12:55:17 +0200

    Modify constructor to set private member variable

--- Inherittance/Sample_Inherit.java
+++ Inherittance/Sample_Inherit.java
@@ -2,7 +2,7 @@
  * @Author: MD Atif Bin Karim
  * @Date:   01-01-1970 01:00:00
  * @Last Modified by:   MD Atif Bin Karim
- * @Last Modified time: 16-04-2023 12:20:43
+ * @Last Modified time: 16-04-2023 12:54:36
  */
 
 // Without any specifier class is private in Java. Insane
@@ -30,12 +30,17 @@ class Child extends Parent{
 
     // Private member variable of child class
     private int childage;
+    private String childname;
 
     // Constructor of Child class
-    public Child(int age_){
+    public Child(int age_, String childname_){
 
+        // Remember, at first call or set super.
         // Calling constructor of parent class
         super(age_);
+
+        // set private member of child class in it's constructor 
+        childname = childname_;
     }
 
     // This function calls the function from Parent class
@@ -55,6 +60,12 @@ class Child extends Parent{
     public int childAge(){
         return childage;
     }
+
+    // Get method for childname variable
+    public String childName(){
+        System.out.print("Child class: childname: ");
+        return childname;
+    }
 }
 
 public class Sample_Inherit {
@@ -64,12 +75,15 @@ public class Sample_Inherit {
         par_obj.displayAge();
         par_obj.displayAgeMore();
 
-        Child chl_obj = new Child(12);
+        Child chl_obj = new Child(12, "BD");
         chl_obj.displayAgeChild();
 
         // Calling set get method of child age
         chl_obj.childAge(5);
         System.out.println("childage: " + chl_obj.childAge());
+        
+        // calling get methid of child class to get childname
+        System.out.println(chl_obj.childName());
         
     }
 
