bc84eba810f8388cd6dd3525f730d5e5c5e2489a
atifkarim <atif0902082@gmail.com>
2023-04-16 15:36:29 +0200

    Add function in Language class

--- Abstract_class/Abstract.java
+++ Abstract_class/Abstract.java
@@ -2,13 +2,17 @@
  * @Author: MD Atif Bin Karim
  * @Date:   01-01-1970 01:00:00
  * @Last Modified by:   MD Atif Bin Karim
- * @Last Modified time: 16-04-2023 15:23:40
+ * @Last Modified time: 16-04-2023 15:36:00
  * @Brief: This file shows the abstract class property
  */
 package Abstract_class;
 
 abstract class Language {
     abstract void printLanguage();
+
+    public void doCommunicate(String name){
+        System.out.println("Language class communicates with " + name);
+    }
 }
 
 class Dialect extends Language{
@@ -20,13 +24,15 @@ class Dialect extends Language{
 public class Abstract extends Language{
 
     public void printLanguage(){
-        System.out.println("Hi");
+        System.out.println("Hi i am abstract class");
     }
     public static void main(String[] args) {
         Abstract obj = new Abstract();
         obj.printLanguage();
+        obj.doCommunicate("Abstract class");
 
         Dialect dialect = new Dialect();
         dialect.printLanguage();
+        dialect.doCommunicate("Dialect class");
     }
 }
